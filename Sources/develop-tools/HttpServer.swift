af5181f5aa5d0e659a938b3643220adec8a38ce9
chenxi <991001506@qq.com>
2022-07-31 17:57:15 +0800

    refactor http server

--- Sources/develop-tools/HttpServer.swift
+++ Sources/develop-tools/HttpServer.swift
@@ -23,82 +23,13 @@ struct HttpServer: ParsableCommand {
     
     mutating func run() throws {
         logger.info("🌍 Starting web server at http://localhost:\(port)")
-        logger.info("Press ENTER to stop the server and exit")
+        
         /**
          python3 -m http.server
          python -m SimpleHTTPServer
          ruby -run -e httpd . -p
          */
-        let runner = WebsiteRunner(portNumber: port)
+        let runner = WebsiteRunner(portNumber: port, logger: logger)
         try runner.run()
-    }
-    
-    
-}
-
-internal struct WebsiteRunner {
-    let portNumber: Int
-    
-    func run() throws {
-        let serverQueue = DispatchQueue(label: "Publish.WebServer")
-        let serverProcess = Process()
-        
-        serverQueue.async {
-            do {
-                if isPython3Exist() {
-                    _ = try ExecuteCommand(
-                        command: "python3 -m http.server \(self.portNumber)",
-                        process: serverProcess
-                    )
-                } else if isPython2Exist() {
-                    _ = try ExecuteCommand(
-                        command: "python -m SimpleHTTPServer \(self.portNumber)",
-                        process: serverProcess
-                    )
-                } else if isRubyExist() {
-                    _ = try ExecuteCommand(
-                        command: "ruby -run -e httpd . -p\(self.portNumber)",
-                        process: serverProcess
-                    )
-                }
-            } catch let error as ShellError {
-                print(error.message)
-            } catch {
-                print(error.localizedDescription)
-            }
-
-            serverProcess.terminate()
-            exit(1)
-        }
-
-        _ = readLine()
-        serverProcess.terminate()
-    }
-    
-    private func isPython3Exist() -> Bool {
-        do {
-            _ = try ExecuteCommand(command: "which python3")
-            return true
-        } catch {
-            return false
-        }
-    }
-    
-    private func isPython2Exist() -> Bool {
-        do {
-            _ = try ExecuteCommand(command: "which python")
-            return true
-        } catch {
-            return false
-        }
-    }
-    
-    private func isRubyExist() -> Bool {
-        do {
-            _ = try ExecuteCommand(command: "which ruby")
-            return true
-        } catch {
-            return false
-        }
     }
 }
---
+++ Sources/develop-tools/WebsiteRunner.swift
@@ -0,0 +1,81 @@
+//
+//  WebsiteRunner.swift
+//  
+//
+//  Created by 陈希 on 2022/7/31.
+//
+
+import Foundation
+
+internal struct WebsiteRunner {
+    let portNumber: Int
+    let logger: Logging
+    
+    func run() throws {
+        let serverQueue = DispatchQueue(label: "Publish.WebServer")
+        let serverProcess = Process()
+        let command = self.command
+        
+        if command.isEmpty {
+            logger.warning("not find python3/python/ruby environment!")
+            return
+        }
+        logger.info("execute command: \(command)")
+        logger.info("Press ENTER to stop the server and exit")
+        
+        serverQueue.async {
+            do {
+                _ = try ExecuteCommand(command: command, process: serverProcess)
+            } catch let error as ShellError {
+                print(error.message)
+            } catch {
+                print(error.localizedDescription)
+            }
+
+            serverProcess.terminate()
+            exit(1)
+        }
+
+        let input = readLine()
+        logger.debug("received [\(input ?? "")], terminated.")
+        serverProcess.terminate()
+    }
+    
+    var command: String {
+        if isPython3Exist() {
+            return "python3 -m http.server \(portNumber)"
+        } else if isPython2Exist() {
+            return "python -m SimpleHTTPServer \(portNumber)"
+        } else if isRubyExist() {
+            return "ruby -run -e httpd . -p\(portNumber)"
+        }
+        return ""
+    }
+   
+    private func isPython3Exist() -> Bool {
+        do {
+            _ = try ExecuteCommand(command: "which python3")
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    private func isPython2Exist() -> Bool {
+        do {
+            _ = try ExecuteCommand(command: "which python")
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    private func isRubyExist() -> Bool {
+        do {
+            _ = try ExecuteCommand(command: "which ruby")
+            return true
+        } catch {
+            return false
+        }
+    }
+}
